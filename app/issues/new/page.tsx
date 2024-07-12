863aa86e601ae9fd7111cd178b650a5ecfdf3181
lyesinfo2024 <lyesinformatique26@gmail.com>
2024-07-12 19:07:15 +0100

    Handle Errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,12 @@
 "use client";
-import { Button, TextField } from "@radix-ui/themes";
-import { useForm, Controller } from "react-hook-form";
+import { Button, Callout, TextArea, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { CiCircleInfo } from "react-icons/ci";
 
 interface IssueForm {
   title: string;
@@ -12,36 +14,54 @@ interface IssueForm {
 }
 
 const NewIssuePage = () => {
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const { register, handleSubmit, control } = useForm<IssueForm>();
   const router = useRouter();
-  console.log(register("title"));
+  const [error, setError] = useState("");
   return (
-    <form
-      className="space-y-3 max-w-xl"
-      onSubmit={handleSubmit(async (data) => {
-        // await axios.post("/api/issues", data);
-        // router.push("/issues");
+    <div className="max-w-xl ">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Icon>
+            <CiCircleInfo />
+          </Callout.Icon>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3 "
+        onSubmit={handleSubmit(async (data) => {
+          // 1 ======> ************** use axios methde ********************
 
-        await fetch("/api/issues", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(data),
-        });
-        router.push("/");
-      })}
-    >
-      <TextField.Root placeholder="title" {...register("title")} />
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-      />
-      <Button>Submit Issue</Button>
-    </form>
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("An unexpected error has occured!");
+          }
+
+          // 2 ======> ************** use fetch methde ********************
+          // await fetch("/api/issues", {
+          //   method: "POST",
+          //   headers: {
+          //     "Content-Type": "application/json",
+          //   },
+          //   body: JSON.stringify(data),
+          // });
+          // router.push("/issues");
+        })}
+      >
+        <TextField.Root placeholder="title" {...register("title")} />
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+
+        <Button>Submit Issue</Button>
+      </form>
+    </div>
   );
 };
 
